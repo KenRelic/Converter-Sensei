6f83123b39cdb0db2f34fd74084d002bf319b174
KenRelic <simeonkenneth3@gmail.com>
2020-09-22 14:22:31 +0100

    refactor and design : romanNumeral converter page, Base converter page

---
+++ public/data.js
@@ -0,0 +1,78 @@
+let convData = {
+    length: {
+      name: 'length',
+      desc: 'Length is a measure of distance. It is the term used for identifying the size of an object or distance from one point to to another.',
+      types: ['meter', 'centimeter', 'kilometer', 'nanometer', 'millimeter', 'inch', 'foot', 'mile', 'yard',
+        'micrometer'
+      ],
+      SIUnit: ['m', 'cm', 'km', 'nm', 'mm', 'in', 'ft', 'mi', 'yd', 'μm'],
+    },
+    mass: {
+      name: 'mass',
+      desc: 'Mass is a measure of the amount of matter in an object. It is both a property of a physical body and a measure of its resistance to acceleration (a change in its state of motion) when a net force is applied.',
+      types: ['kilogram', 'gram', 'ounce', 'tonne', 'carat', 'milligram', 'pound', 'stone', 'short ton', 'long ton',
+        'microgram'
+      ],
+      SIUnit: ['kg', 'g', 'oz', 't', 'ct', 'mg', 'lb', 'st', 'sh tn', 'ton', 'μg']
+    },
+    time: {
+      name: 'time',
+      desc: 'Time can be defined as the ongoing and continuous sequence of events that occur in succession, from the past through the present to the future.',
+      types: ['second', 'minute', 'hour', 'day', 'microsecond', 'millisecond', 'week', 'month', 'year', 'decade',
+        'moment', 'jiffy'
+      ],
+      SIUnit: ['s', 'min', 'h', 'd', 'μs', 'ms', 'wk', 'mo', 'yr', 'dec', '', ''],
+    },
+    area: {
+      name: 'area',
+      desc: 'Area is the quantity that expresses the space occupied by a two-dimensional figure or shape ',
+      types: ['acre', 'hectare', 'squareMeter', 'squareKilometer', 'squareFoot', 'squareInch', 'squareYard',
+        'squareMile'
+      ],
+      SIUnit: ['ac', 'ha', 'sq m', 'sq km', 'sq ft', 'sq in', 'sq yd', 'sq mi']
+    },
+    temperature: {
+      name: 'temperature',
+      desc: 'Temperature is a physical quantity that expresses hot and cold. Temperature is measured with a thermometer.',
+      types: ['celcius', 'fahrenheit', 'rankine', 'kelvin', 'delisle', 'newton', 'réaumur'],
+      SIUnit: ['°C', '°F', '°R', 'K', '°De', '°N', '°Ré']
+    },
+    pressure: {
+      name: 'pressure',
+      desc: 'Pressure is the force applied perpendicular to the surface of an object per unit area over which that force is distributed.',
+      types: ['pascal', 'torr', 'bar', 'atmosphere(std)'],
+      SIUnit: ['Pa', 'torr', 'bar', 'atm']
+    },
+    volume: {
+      name: 'volume',
+      desc: 'Volume is the quantity of three-dimensional space enclosed by a closed surface, for example, the space that a substance (solid, liquid, gas, or plasma) or shape occupies or contains. Volume is often quantified numerically using the SI derived unit, the cubic metre.',
+      types: ['cubic-meter', 'litre', 'gallon(imp)', 'millilitre', 'ounce(fluid)', 'cubicfoot', 'cubicInch'],
+      SIUnit: ['m³', 'L', 'gal', 'mL', 'fl oz', 'ft³', 'in³']
+    },
+    energy: {
+      name: 'energy',
+      desc: 'Energy is the quantitative property that must be transferred to an object in order to perform work on, or to heat,',
+      types: ['joule', 'kilojoule', 'gramcalorie', 'kilocalorie', 'electronvolt', 'watthour', 'footpound',
+        'kilowatt-hour'
+      ],
+      SIUnit: ['J', 'kJ', 'gcal', 'kcal', 'eV', 'Wh', 'ft-lb', 'kWh']
+    },
+    storage: {
+      name: 'storage',
+      desc: 'Computer data storage is a technology consisting of computer components and recording media that are used to retain digital data. Digital storage units are the ways computer data is expressed. They are typically articulated using bytes and usually abbreviated in expression.',
+      types: ['bit', 'byte', 'kilobit', 'kibibit', 'kilobyte', 'kibibyte', 'megabyte', 'mebibyte', 'gigabyte',
+        'gibibyte', 'terabyte', 'tebibyte', 'petabyte', 'pebibyte'
+      ],
+      SIUnit: ['b', 'B', 'Kb', 'Kib', 'KB', 'KiB', 'MB', 'MiB', 'GB', 'GiB', 'TB', 'TiB', 'PB', 'PiB']
+    },
+  };
+
+  let baseData = {
+    'base': {
+      name:'base',
+      desc: 'A number base is the number of digits or combination of digits that a system of counting uses to represent numbers. A base can be any whole number greater than 0. The most commonly used number system is the decimal system, commonly known as base 10.',
+      types: ['decimal', 'binary', 'octal', 'hexadecimal'],
+      'inputType': ['number', 'number', 'number', 'text'],
+    },
+  }
+  export {convData, baseData};
---
+++ public/main.js
@@ -0,0 +1,313 @@
+import { convData, baseData } from './data.js';
+
+let menuBar = document.getElementById('menu-bar');
+let currentPage = document.getElementById('current-page');
+let menuBgColors = {
+  "unit-conv": "#df3f05",
+  "rom-conv": "#c40038",
+  "base-conv": "#020c72",
+  "other-conv": "",
+}
+let allCards = document.querySelectorAll('.card');
+let mainArea = document.getElementById('main');
+let backBtns = document.querySelectorAll('.back-btn');
+let toggleSwitch = document.getElementById('toggle-icon');
+let unitsSection = document.querySelector('.units-section');
+let unitUnitFrom = document.querySelector('#unit-conv .to-unit');
+let unitUnitTo = document.querySelector('#unit-conv .from-unit');
+let baseUnitFrom = document.querySelector('#base-conv .to-unit');
+let baseUnitTo = document.querySelector('#base-conv .from-unit');
+let switchUnitCoversionBtn = document.querySelector('#unit-conv .switch-conv');
+let switchBaseConversionBtn = document.querySelector('#base-conv .switch-conv');
+let unitConversonType = document.querySelector('.conv-type-wrapper');
+
+let unitToSelect = '';
+let selectedUnitType = '';
+let currentRomConv = 'num2Rom';
+
+unitUnitFrom.addEventListener('click', showListOfUnits);
+unitUnitTo.addEventListener('click', showListOfUnits);
+baseUnitFrom.addEventListener('click', showListOfUnits);
+baseUnitTo.addEventListener('click', showListOfUnits);
+
+toggleSwitch.addEventListener('click', toggleMode);
+switchUnitCoversionBtn.addEventListener('click', switchConversionUnits);
+switchBaseConversionBtn.addEventListener('click', switchConversionUnits);
+
+backBtns.forEach(btn => btn.addEventListener('click', goBack));
+
+window.onload = function () {
+  mainArea.addEventListener('click', selectConverter);
+};
+
+function toggleMode() {
+  let rootEl = document.querySelector(':root');
+  let toggleSwitch = document.getElementById('toggle-switch');
+  let currentMode = toggleSwitch.classList.contains('fa-toggle-on');
+  if (currentMode) {
+    toggleSwitch.classList.replace('fa-toggle-on', 'fa-toggle-off')
+    return rootEl.style = `--page-title: #fff; --text:#000;
+              --unit-bg:#fff; --rom-bg:#fff;  --base-bg: #fff; --unit-text:#000; --bg: #fff;`
+  }
+  toggleSwitch.classList.replace('fa-toggle-off', 'fa-toggle-on')
+  return rootEl.style = `--page-title: #000;--text:#fff;
+          --unit-bg:#611d00;--rom-bg:#610027;  --base-bg: #002f61;  --unit-text:#fff;      --bg: #000031;`
+};
+
+function goBack() {
+  selectedUnitType = '';
+  closeUnitSelectionWrapper()
+  hideConversionPage(this.parentElement)
+  allCards.forEach(card => normalize(card));
+  mainArea.style = 'overflow-y:auto;padding:1em 1em 64px 1em';
+  return mainArea.addEventListener('click', selectConverter);
+};
+
+function growDiv(div) {
+  div.style = `width:100%; height: 97%; display: flex; cursor: unset; pointer-events:none`;
+  // div.children[0].style = 'display:initial; pointer-events:visible';
+  div.children[3].style.display = 'flex';
+  setTimeout(() => {
+    showConversionPage(div)
+  }, 3000);
+};
+
+function normalize(div) {
+  div.style = `width:45%; height: 30%; display: flex; cursor:pointer; overflow: hidden;`;
+  div.children[0].style = 'display:none;pointer-events:none';
+  div.children[3].style.display = 'none';
+  // setTimeout(()=>{div.style.flex = 'none'},5000)
+};
+
+function shrinkDiv(div) {
+  div.style = `width:0%; height: 0%; display: none; `;
+};
+
+function selectConverter(event) {
+  let id = event.target.id;
+  if (id !== 'main' && !event.target.classList.contains('back-btn')) {
+    menuBar.style.backgroundColor = `${menuBgColors[id]}`;
+    currentPage.style.color = `${menuBgColors[id]}`;
+    allCards.forEach(card => {
+      if (card.id !== id) shrinkDiv(card);
+    });
+    // if(id == 'base-conv') createConvTypeUnits('base-conv');
+    mainArea.removeEventListener('click', selectConverter)
+    return growDiv(document.getElementById(id));
+  }
+};
+
+function showConversionPage(div) {
+  mainArea.style = 'padding:0;overflow-y:hidden;';
+  div.children[0].style = 'display:initial; pointer-events:visible';
+  div.children[1].style.display = 'none';
+  div.children[2].style.display = 'none';
+  div.children[3].style.display = 'none';
+  div.style = `width:100%;height:100%;overflow-x:auto; cursor:default`;
+  if (window.matchMedia('(max-width:800px)').matches) {
+    document.querySelector(`#${div.id} .conversion-section`).style.margin = '2em auto 6em auto';
+  }
+  showPageContents(div);
+};
+
+function hideConversionPage(div) {
+  div.children[0].style = 'none';
+  div.children[1].style.display = 'initial';
+  div.children[2].style.display = 'block';
+  div.children[3].style.display = 'none';
+  hidePageContents(div);
+};
+
+function showPageContents(div) {
+  document.getElementById(`${div.id}-page`).style.display = 'block';
+  if (div.id == 'unit-conv') populateUIWithConvType('length', div.id);
+  else if (div.id == 'base-conv') populateUIWithConvType('base', div.id);
+  // else if (div.id == 'rom-conv') switchRomanConvType('');
+};
+
+function hidePageContents(div) {
+  document.getElementById(`${div.id}-page`).style.display = 'none';
+};
+
+unitConversonType.addEventListener('click', (event) => {
+  let convType = event.target.getAttribute('data-conv-type')
+  if (convType !== undefined && convType !== '') {
+    // console.log(convType, event.target.parentElement.parentElement.id)
+    populateUIWithConvType(convType, event.target.parentElement.parentElement.id);
+  }
+})
+
+function populateUIWithConvType(convType, div) {
+  let convTypeName = document.querySelector(`#${div} .unit-intro-note header`) ||
+    '';
+  let convDefinition = document.querySelector(`#${div} .unit-note`) || '';
+  let convDefaultValue1 = document.querySelector(`#${div} .from-unit`).firstChild;
+  let convDefaultValue2 = document.querySelector(`#${div} .to-unit`).firstChild;
+  let convDefaultValue1SIUnit = document.querySelectorAll(`#${div} .unit-icon`)[0] || '';  //
+  let convDefaultValue2SIUnit = document.querySelectorAll(`#${div} .unit-icon`)[1] || '';   //
+  let el = document.querySelector(`[data-conv-type="${convType}"]`) ||
+    '';
+
+  selectedUnitType = convType;
+  convTypeName ? convTypeName.textContent = convType : '';
+  convDefinition ? div == 'base-conv' ? convDefinition.textContent = baseData[convType].desc :
+    convDefinition.textContent = convData[convType].desc : '';
+
+  if (div == 'base-conv') {
+    convDefaultValue1.replaceWith(baseData[convType].types[0]);
+    convDefaultValue2.replaceWith(baseData[convType].types[1]);
+    document.querySelector(`#${div} .from-unit`).nextElementSibling.setAttribute('type',
+      baseData[convType].inputType[0]);
+    document.querySelector(`#${div} .to-unit`).nextElementSibling.setAttribute('type',
+      baseData[convType].inputType[3]);
+  } else {
+    convDefaultValue1.replaceWith(convData[convType].types[0]);
+    convDefaultValue2.replaceWith(convData[convType].types[1]);
+  }
+
+  if (convDefaultValue1SIUnit && convDefaultValue2SIUnit) {
+    convDefaultValue1SIUnit.textContent = convData[convType].SIUnit[0];
+    convDefaultValue2SIUnit.textContent = convData[convType].SIUnit[1];
+  }
+
+  if (el) {
+    let parentEl = el.parentElement;
+    for (let i = 0; i < parentEl.childElementCount; i += 1) {
+      if (parentEl.children[i].getAttribute('data-conv-type') == convType) {
+        parentEl.children[i].classList.add('opacity');
+        parentEl.children[i].style = `pointer-events:default;cursor:none;`;
+      } else {
+        parentEl.children[i].classList.remove('opacity');
+        parentEl.children[i].style = `pointer-events:visible;cursor:pointer;`;
+      }
+    }
+  }
+  createConvTypeUnits(convType);
+};
+
+function showListOfUnits(event) {
+  setTimeout(() => {
+    unitsSection.style.bottom = '3em';
+  }, 400);
+  unitsSection.style.display = 'flex';
+  unitToSelect = event.target.classList[0];
+};
+
+function createConvTypeUnits(convType) {
+  let contents = '';
+  let convTypeData = convData[convType] || baseData[convType];
+  for (let i = 0; i < convTypeData.types.length; i += 1) {
+    contents +=
+      `<p class="selectable-unit" data-unit="${convTypeData.types[i]}">${convTypeData.types[i]}</p>`
+  }
+  contents += `<span class="fa fa-times close-unit-section-btn" aria-label="close selection"></span>`;
+  unitsSection.innerHTML = contents;
+
+
+  setTimeout(() => {
+    let closeUnitSelectionWrapperBtn = document.querySelector('.close-unit-section-btn');
+    let selectableUnits = document.querySelectorAll('.selectable-unit');
+    selectableUnits.forEach(unit => unit.addEventListener('click', (event) => {
+      let el = event.target;
+      let index;
+      
+      if (convTypeData.name !== 'base') {
+        index = convData[selectedUnitType].types.indexOf(`${el.textContent}`);
+        document.querySelector(`.${unitToSelect}`).parentElement.children[0].textContent =
+          convData[selectedUnitType].SIUnit[index]
+        document.querySelector(`.${unitToSelect}`).firstChild.replaceWith(`${el.textContent}`);
+
+      } else {
+        document.querySelector(`#base-conv .${unitToSelect}`).firstChild.replaceWith(`${el.textContent}`);
+      }
+
+      return closeUnitSelectionWrapper();
+    }))
+    closeUnitSelectionWrapperBtn.addEventListener('click', closeUnitSelectionWrapper)
+  }, 2000);
+};
+
+function closeUnitSelectionWrapper() {
+  setTimeout(() => {
+    unitsSection.style.display = 'none';
+  }, 400);
+  unitsSection.style.bottom = '-102%';
+};
+
+function switchConversionUnits() {
+  let fromUnit, toUnit, fromSIUnit, toSIUnit, toSIValue, fromSIValue, toValue, fromValue;
+  if (this.parentElement.parentElement.id == 'base-page') {
+    fromUnit = this.previousElementSibling.children[0];
+    toUnit = this.nextElementSibling.children[0];
+  } else {
+    fromUnit = this.previousElementSibling.children[1];
+    toUnit = this.nextElementSibling.children[1];
+    fromSIUnit = this.previousElementSibling.children[0];
+    toSIUnit = this.nextElementSibling.children[0];
+
+    toSIValue = fromSIUnit.textContent;
+    fromSIValue = toSIUnit.textContent;
+  }
+
+  //switch values
+  toValue = fromUnit.firstChild.data;
+  fromValue = toUnit.firstChild.data;
+
+  //switch
+  fromUnit.firstChild.replaceWith(fromValue);
+  toUnit.firstChild.replaceWith(toValue);
+  toSIUnit ? toSIUnit.textContent = toSIValue : '';
+  fromSIUnit ? fromSIUnit.textContent = fromSIValue : '';
+
+  //convert unit funtion
+};
+
+let num2RomanConvOption = document.querySelectorAll('#rom-conv .conv-type')[0]
+let roman2NumConvOption = document.querySelectorAll('#rom-conv .conv-type')[1]
+
+function switchRomanConvType() {
+  let convType = this ? this.getAttribute('data-conv-type') : '' || '';
+  switch (convType) {
+    case 'rom2Num': switchValues(roman2NumConvOption, num2RomanConvOption, convType)
+      break;
+    case 'num2Rom': switchValues(num2RomanConvOption, roman2NumConvOption, convType);
+      break;
+    default:
+      break;
+  }
+}
+function switchValues(selectedOption, otherOption, convType) {
+  let fromName = selectedOption.parentElement.nextElementSibling.children[0].children[0];
+  let fromInput = selectedOption.parentElement.nextElementSibling.children[0].children[1];
+  let toName = selectedOption.parentElement.nextElementSibling.children[2].children[0];
+  let toInput = selectedOption.parentElement.nextElementSibling.children[2].children[1];
+  let switchedToName = fromName.textContent;
+  let switchedFromName = toName.textContent;
+  let switchedFromInputType = toInput.getAttribute('type');
+  let switchedToInputType = fromInput.getAttribute('type');
+  let switchedFromInputDataValue = toInput.getAttribute('data-name');
+  let switchedToInputDataValue = fromInput.getAttribute('data-name');
+
+  if (convType !== currentRomConv) {
+    fromName.textContent = switchedFromName;
+    toName.textContent = switchedToName;
+    fromInput.setAttribute('type', switchedFromInputType);
+    fromInput.setAttribute('data-name', switchedFromInputDataValue);
+    toInput.setAttribute('type', switchedToInputType);
+    toInput.setAttribute('data-name', switchedToInputDataValue);
+    toInput.value = '';
+    fromInput.value = '';
+    currentRomConv = convType;
+  }
+
+
+  selectedOption.classList.add('opacity');
+  otherOption.classList.remove('opacity');
+
+}
+num2RomanConvOption.addEventListener('click', switchRomanConvType);
+roman2NumConvOption.addEventListener('click', switchRomanConvType);
+
+// add the data-name for each input box for beter calculation data-name ='length-meter'
+//split the data value, use the first aprt to get th type of conversion then the second to get the
+//value of conversion.
